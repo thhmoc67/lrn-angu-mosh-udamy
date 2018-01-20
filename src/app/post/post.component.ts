1f7f6748ad3de6df4d29bc8fec450ba58ff697a5
thhmoc67 <dayalsaini67@gmail.com>
2018-01-20 18:12:13 +0530

    update data using put or patch method

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -22,11 +22,19 @@ export class PostComponent {
      input.value='';
      this.http.post(this.url,JSON.stringify(post))
       .subscribe(response =>{
-        post['id']= response.json().id;
+        post.id= response.json().id;
         this.posts.splice(0,0,post);
        
         
     });
+   }
+
+   updatePost(post){
+    this.http.patch(this.url + '/' + post.id ,JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response.json());
+      })
+    //this.http.put(this.url,JSON.stringify(post))
    }
 
 }
